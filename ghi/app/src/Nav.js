9b35637fa43232e41961af6ef36651b7f6d0d69f
Ray <charlesraycowan@yahoo.com>
2023-06-12 15:19:43 -0400

    further nav-bar enhancement

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -14,14 +14,14 @@ function Nav() {
               <button class="btn text-white dropdown-toggle bg-success" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                 Sales Department
               </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <li><NavLink className="dropdown-item" aria-current="page" to="/salesperson/">List of Salespeople</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/">List of Customers</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/">List of Sales</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/history">Sales History</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/salesperson/create/">Add a Salesperson</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/create/">Add a Customer</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/create/">Add a Sale</NavLink></li>
+              <ul class="dropdown-menu bg-success" aria-labelledby="dropdownMenuButton">
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/salesperson/">List of Salespeople</NavLink></li>
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/customers/">List of Customers</NavLink></li>
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/sales/">List of Sales</NavLink></li>
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/sales/history">Sales History</NavLink></li>
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/salesperson/create/">Add a Salesperson</NavLink></li>
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/customers/create/">Add a Customer</NavLink></li>
+                <li><NavLink className="dropdown-item text-white" aria-current="page" to="/sales/create/">Add a Sale</NavLink></li>
               </ul>
             </div>
 
@@ -30,13 +30,13 @@ function Nav() {
                 <button class="btn text-white dropdown-toggle bg-success" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                   Inventory Department
                 </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/manufacturers/">List of Manufacturers</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/automobiles/">List of Automobiles</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/models/">List of Vehicle Models</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/manufacturers/create/">Add a Manufacturer</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/automobiles/create/">Add an Automobile</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/models/create/">Add a Vehicle Model</NavLink></li>
+                <ul class="dropdown-menu bg-success" aria-labelledby="dropdownMenuButton">
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/manufacturers/">List of Manufacturers</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/automobiles/">List of Automobiles</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/models/">List of Vehicle Models</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/manufacturers/create/">Add a Manufacturer</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/automobiles/create/">Add an Automobile</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/models/create/">Add a Vehicle Model</NavLink></li>
                 </ul>
               </div>
 
@@ -44,11 +44,11 @@ function Nav() {
                 <button class="btn text-white dropdown-toggle bg-success" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                   Service Department
                 </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/technicians/">List of Technicians</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/appointments/history/">Service History</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/technicians/create/">Add a Technician</NavLink></li>
-                  <li><NavLink className="dropdown-item" aria-current="page" to="/appointments/create">Add an Appointment</NavLink></li>
+                <ul class="dropdown-menu bg-success" aria-labelledby="dropdownMenuButton">
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/technicians/">List of Technicians</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/appointments/history/">Service History</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/technicians/create/">Add a Technician</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" aria-current="page" to="/appointments/create">Add an Appointment</NavLink></li>
                 </ul>
               </div>
 
