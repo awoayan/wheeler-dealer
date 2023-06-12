df7b689f306040b8e0a8916f8291c1d462df3946
Ray <charlesraycowan@yahoo.com>
2023-06-12 15:13:09 -0400

    refactored nav-bar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,135 +9,53 @@ function Nav() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li>
-              <div className="d-flex">
-                <button id="sales-btn" className="navbar-toggler navbar-dropdown d-inline" type="button" data-bs-toggle="collapse" data-bs-target="#sales" aria-controls="sales" aria-expanded="false" aria-label="Toggle navigation">
-                  <div className="navbar-brand navbar-dropdown">Sales Department</div>
+
+            <div class="btn-group">
+              <button class="btn text-white dropdown-toggle bg-success" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Sales Department
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><NavLink className="dropdown-item" aria-current="page" to="/salesperson/">List of Salespeople</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/">List of Customers</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/">List of Sales</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/history">Sales History</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/salesperson/create/">Add a Salesperson</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/create/">Add a Customer</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/create/">Add a Sale</NavLink></li>
+              </ul>
+            </div>
+
+
+              <div class="btn-group">
+                <button class="btn text-white dropdown-toggle bg-success" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                  Inventory Department
                 </button>
-                <div className="collapse" id="sales">
-                  <div className="row">
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/salesperson/">
-                        List of Salespeople
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/customers/">
-                        List of Customers
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/sales/">
-                        List of Sales
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/sales/history">
-                        Sales History
-                      </NavLink>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/salesperson/create/">
-                        Add a Salesperson
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/customers/create/">
-                        Add a Customer
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/sales/create/">
-                        Add a Sale
-                      </NavLink>
-                    </div>
-                  </div>
-                </div>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/manufacturers/">List of Manufacturers</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/automobiles/">List of Automobiles</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/models/">List of Vehicle Models</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/manufacturers/create/">Add a Manufacturer</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/automobiles/create/">Add an Automobile</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/models/create/">Add a Vehicle Model</NavLink></li>
+                </ul>
               </div>
-            </li>
-            <li>
-              <div className="d-flex">
-                <button id="inv-btn" className="navbar-toggler navbar-dropdown d-inline" type="button" data-bs-toggle="collapse" data-bs-target="#inventory" aria-controls="inventory" aria-expanded="false" aria-label="Toggle navigation">
-                  <div className="navbar-brand navbar-dropdown">Inventory Department</div>
+
+              <div class="btn-group">
+                <button class="btn text-white dropdown-toggle bg-success" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                  Service Department
                 </button>
-                <div className="collapse" id="inventory">
-                  <div className="row">
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/manufacturers/">
-                        List of Manufacturers
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/automobiles/">
-                        List of Automobiles
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/models/">
-                        List of Vehicle Models
-                      </NavLink>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/manufacturers/create/">
-                        Add a Manufacturer
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/automobiles/create/">
-                        Add an Automobile
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/models/create/">
-                        Add a Vehicle Model
-                      </NavLink>
-                    </div>
-                  </div>
-                </div>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/technicians/">List of Technicians</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/appointments/history/">Service History</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/technicians/create/">Add a Technician</NavLink></li>
+                  <li><NavLink className="dropdown-item" aria-current="page" to="/appointments/create">Add an Appointment</NavLink></li>
+                </ul>
               </div>
-            </li>
-            <li>
-              <div className="d-flex">
-                <button id="serv-btn" className="navbar-toggler navbar-dropdown d-inline" type="button" data-bs-toggle="collapse" data-bs-target="#services" aria-controls="services" aria-expanded="false" aria-label="Toggle navigation">
-                  <div className="navbar-brand navbar-dropdown">Services Department</div>
-                </button>
-                <div className="collapse" id="services">
-                  <div className="row">
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/technicians/">
-                        List of Technicians
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/appointments/history/">
-                        Service History
-                      </NavLink>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/technicians/create/">
-                        Add a Technician
-                      </NavLink>
-                    </div>
-                    <div className="col ps-3 pe-3 mx-3">
-                      <NavLink className="nav-link" aria-current="page" to="/appointments/create">
-                        Add an Appointment
-                      </NavLink>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+
+
+          </div>
+          </div>
+          </nav>
   )
 }
 
